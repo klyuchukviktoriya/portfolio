aed3cd56ad8af9420d7c0251b2476d1b95e85dbf
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2025-12-22 01:52:41 +0200

    about draft

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import PageWrapper from "@/components/PageWrapper/PageWrapper";
 import Hero from "@/components/Hero/Hero";
-// import About from "@/components/About/About";
+import About from "@/components/About/About";
+import Skills from "@/components/Skills/Skills";
 
 export default function HomePage() {
   return (
     <>
       <PageWrapper>
         <Hero />
-        {/*  <About /> */}
+        <About />
+        <Skills />
       </PageWrapper>
     </>
   );
