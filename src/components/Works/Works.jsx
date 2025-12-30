008d2a0173bcc7bb03a3732a38c77ab95f244e90
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2025-12-30 22:15:49 +0200

    chore: ignore compiled css and sourcemaps

---
+++ src/components/Works/Works.jsx
@@ -0,0 +1,9 @@
+import css from "./Works.module.scss";
+
+export default function Works() {
+  return (
+    <div>
+      <h2 className={css.visuallyHidden}>works</h2>
+    </div>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import PageWrapper from "@/components/PageWrapper/PageWrapper";
 import Hero from "@/components/Hero/Hero";
 import About from "@/components/About/About";
 import Skills from "@/components/Skills/Skills";
+import Works from "@/components/Works/Works";
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
         <Hero />
         <About />
         <Skills />
+        <Works />
       </PageWrapper>
     </>
   );
