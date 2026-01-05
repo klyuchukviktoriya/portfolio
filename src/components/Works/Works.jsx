27c23aa458db2a1a58109d881b05fb05e1a280c3
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2026-01-05 22:11:49 +0200

    works draft

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -2,8 +2,12 @@ import css from "./Works.module.scss";
 
 export default function Works() {
   return (
-    <div>
-      <h2 className={css.visuallyHidden}>works</h2>
-    </div>
+    <section className={css.works}>
+      <div className={css.works__container}>
+        <h2 className={css.visuallyHidden}>works</h2>
+        <div>Карусель</div>
+        <button>check my works</button>
+      </div>
+    </section>
   );
 }
