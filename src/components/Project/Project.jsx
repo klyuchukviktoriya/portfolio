f333667a86f7bc539a1174b2fd37a65e8177dab2
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2026-01-08 01:13:39 +0200

    draft projects

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,11 +1,47 @@
-export default function Project({ id, img, demo, github }) {
+import css from "./Project.module.scss";
+
+export default function Project({
+  id,
+  img,
+  demo,
+  github,
+  design,
+  name,
+  technologies,
+  description,
+}) {
   return (
-    <li>
-      <div>
+    <div className={css.project}>
+      <div className={css.project__imageWrapper}>
         <img src={`${img}.jpg`} />
         <p>{id}</p>
       </div>
-      <div></div>
-    </li>
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <div className={css.project__details}>
+        <ul>
+          <li>
+            <a href={demo} target="_blank" rel="noreferrer">
+              Live demo
+            </a>
+          </li>
+          <li>
+            <a href={github} target="_blank" rel="noreferrer">
+              GitHub
+            </a>
+          </li>
+        </ul>
+        <ul>
+          {design.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <ul>
+          {technologies.map((technology, index) => (
+            <li key={index}>{technology}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -1,17 +1,15 @@
 import css from "./ProjectsList.module.scss";
-import projects from "../../helpers";
 import Project from "../Project/Project";
+import { projects } from "../../helpers/constants.js";
 
 export default function ProjectsList() {
   return (
     <section className={css.projects}>
       <div className={css.projects__container}>
         <h2>works</h2>
-        <ul>
-          {projects.map(project => {
-            <Project key={project.id} {...project} />;
-          })}
-        </ul>
+        {projects.map(project => (
+          <Project key={project.id} {...project} />
+        ))}
       </div>
     </section>
   );
